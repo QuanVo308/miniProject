76da2191839eebc0f8ef4df8431f1182dc2f8c51
QuanVo308 <minhquan.vo01@gmail.com>
2022-05-20 10:36:38 +0700

    api complete

--- miniProject/urls.py
+++ miniProject/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('generateData/', views.temp, name='temp'),
     path('login/',views.login, name="login"),
-    path('loginAuthen/',views.loginAuthen, name="loginAuthen"),
-    path('logout/',views.Logout, name="logout"),
-    path('register/', views.Register, name="register"),
-    path('registerSubmit/', views.registerSubmit, name="registerSubmit"),
+    path('loginAuthen/',views.login_authen, name="loginAuthen"),
+    path('logout/',views.logout, name="logout"),
+    path('register/', views.register, name="register"),
+    path('registerSubmit/', views.register_submit, name="registerSubmit"),
+    path('get1/', views.get_static_pop_mac, name='get1'),
+    path('get2/', views.get_dynamic_pop_mac, name='get2'),
 
 ]
--- miniProject/views.py
+++ miniProject/views.py
@@ -1,21 +1,26 @@
 from os import popen
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import SampleData
 import random
 from django.shortcuts import render, get_object_or_404, redirect
 from .form import LoginForm  
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import authenticate, logout as auth_logout
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.models import User, Group
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+from django.db.models import Q
 
-
+@csrf_exempt
 def index(request):
+    group = ""
     if request.user.is_authenticated:
-        notice = "User is logged in as %s with a role %s %s" % (request.user.username, request.user.groups.all()[0], request.user.has_perm('miniProject.add_sampledata'))
-        return render(request, 'miniProject/home.html', {'notice':notice})
+        notice = "User is logged in as %s with a role %s" % (request.user.username, request.user.groups.all()[0])
+        group = request.user.groups.all()[0]
+        return render(request, 'miniProject/home.html', {'notice':notice, 'group' : group})
     else:
         notice = "User is not logged in :("
-        return render(request, 'miniProject/home.html', {'notice':notice})
+        return render(request, 'miniProject/home.html', {'notice':notice, 'group' : group})
 
 
 def temp(request):
@@ -30,7 +35,7 @@ def temp(request):
 def login(request):
     return render(request, 'miniProject/login.html')
 
-def loginAuthen(request):
+def login_authen(request):
 
     form = LoginForm(request.POST)
 
@@ -43,14 +48,14 @@ def loginAuthen(request):
         return render(request, 'miniProject/loginResult.html')
             
 
-def Logout(request):
-    logout(request)
+def logout(request):
+    auth_logout(request)
     return render(request, 'miniProject/logout.html')
 
-def Register(request):
+def register(request):
     return render(request, 'miniProject/register.html')
 
-def registerSubmit(request):
+def register_submit(request):
     try:
         user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
         group = Group.objects.get(name = request.POST['accountGroup'])
@@ -59,3 +64,17 @@ def registerSubmit(request):
         return redirect('/home/login')
     except:
         return render(request, "miniProject/registerResult.html")
+
+@csrf_exempt
+def get_static_pop_mac(request):
+    data = SampleData.objects.filter(Q(total_mac__gte = 100) | Q(number_of_pop_tail__gte = 30)).values()
+    print(type(data))
+    return JsonResponse({"data": list(data)})
+
+@csrf_exempt
+def get_dynamic_pop_mac(request):
+    print(request.GET['total_mac'])
+    print(request.GET['number_of_pop_tail'])
+    data = SampleData.objects.filter(total_mac = request.GET['total_mac'], number_of_pop_tail = request.GET['number_of_pop_tail']).values()
+    print(type(data))
+    return JsonResponse({"data": list(data)})
